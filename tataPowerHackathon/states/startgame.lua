2cc0a3bef07ee20459eb864afe2fc34ed3075c29
PranayChavhan <pranay21chavhan@outlook.com>
2023-10-27 19:09:44 +0530

    Start Screens Buttons added

--- tataPowerHackathon/states/startgame.lua
+++ tataPowerHackathon/states/startgame.lua
@@ -12,31 +12,40 @@ local buttonFont = love.graphics.newFont("assets/fonts/EncodeSansSemiCondensed-B
 -- Button properties
 local buttonWidth = 200
 local buttonHeight = 60
+local buttonSpacing = 20
 local buttonX = (love.graphics.getWidth() - buttonWidth) / 2
-local buttonY = (love.graphics.getHeight() - buttonHeight) / 2
-local buttonText = "Start Game"
-local buttonTextColor = {255, 255, 255}
-local buttonColor = {200, 0, 0}
-local shadowColor = {100, 0, 0}
+local startY = (love.graphics.getHeight() - (3 * buttonHeight + 2 * buttonSpacing)) / 2
+
+local buttons = {
+    { text = "Start Game", action = "start" },
+    { text = "Exit Game", action = "exit" },
+    { text = "Instructions", action = "instructions" },
+    { text = "Settings", action = "settings" }
+}
+
 local cornerRadius = 10
 
 function startgame.draw()
-    -- Draw the background image to fill the screen
+    -- Draw the darkened background image
+    love.graphics.setColor(100, 100, 100)
     love.graphics.draw(background, 0, 0, 0, love.graphics.getWidth() / background:getWidth(), love.graphics.getHeight() / background:getHeight())
 
-    -- Draw the button with rounded corners and a shadow
-    love.graphics.setColor(shadowColor)
-    love.graphics.rectangle("fill", buttonX + 10, buttonY + 10, buttonWidth, buttonHeight, cornerRadius, cornerRadius)
-    love.graphics.setColor(buttonColor)
-    love.graphics.rectangle("fill", buttonX, buttonY, buttonWidth, buttonHeight, cornerRadius, cornerRadius)
+    for i, button in ipairs(buttons) do
+        local buttonY = startY + (i - 1) * (buttonHeight + buttonSpacing)
+        
+        -- Set the green background color for each button
+        local buttonColor = {0, 255, 0}
+        love.graphics.setColor(buttonColor)
+        love.graphics.rectangle("fill", buttonX, buttonY, buttonWidth, buttonHeight, cornerRadius, cornerRadius)
 
-    -- Draw the button text in the center
-    local textWidth = buttonFont:getWidth(buttonText)
-    local textX = buttonX + (buttonWidth - textWidth) / 2
-    local textY = buttonY + (buttonHeight - buttonFont:getHeight()) / 2
-    love.graphics.setColor(buttonTextColor)
-    love.graphics.setFont(buttonFont)
-    love.graphics.print(buttonText, textX, textY)
+        -- Draw the button text in the center
+        local textWidth = buttonFont:getWidth(button.text)
+        local textX = buttonX + (buttonWidth - textWidth) / 2
+        local textY = buttonY + (buttonHeight - buttonFont:getHeight()) / 2
+        love.graphics.setColor(255, 255, 255)
+        love.graphics.setFont(buttonFont)
+        love.graphics.print(button.text, textX, textY)
+    end
 end
 
 return startgame
