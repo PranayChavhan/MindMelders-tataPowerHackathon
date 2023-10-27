edb5ff39de5c741087e32599f507ee20826ec260
PranayChavhan <pranay21chavhan@outlook.com>
2023-10-27 17:56:00 +0530

    updated code

--- tataPowerHackathon/main.lua
+++ tataPowerHackathon/main.lua
@@ -4,19 +4,19 @@ local level1 = require("levels.level1") -- Import the Level 1 module
 
 -- Initialize the game
 function love.load()
-    -- Set up the game window
+    -- Set up the game window   
     love.window.setTitle("EcoCraft Challenge")
     
     -- Set full screen mode with the appropriate display width and height
     local displayWidth, displayHeight = love.window.getDesktopDimensions()
-    love.window.setMode(displayWidth, displayHeight, {fullscreen = true})
+    love.window.setMode(displayWidth, displayHeight, {fullscreen = false})
     
-    level1.load() -- Load Level 1
+    -- level1.load() -- Load Level 1
 end
 
 -- Update game logic
 function love.update(dt)
-    level1.update(dt) -- Update Level 1 logic
+    -- level1.update(dt) -- Update Level 1 logic
 end
 
 -- Render the game
