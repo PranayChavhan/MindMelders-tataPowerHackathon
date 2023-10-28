823b54bf90f769b4f614930d01d3304e93e956a0
PranayChavhan <pranay21chavhan@outlook.com>
2023-10-28 14:34:51 +0530

    instruction pages done

--- tataPowerHackathon/src/states/game/Instructions.lua
+++ tataPowerHackathon/src/states/game/Instructions.lua
@@ -3,22 +3,61 @@ Instructions = Class {
 }
 
 function Instructions:init()
-    --self.control = love.graphics.newImage('sprites/control.jpg')
+    self.background = love.graphics.newImage('assets/images/inst.jpg') -- Load your background image
+
+    -- Load a custom font with larger size
+    self.titleFont = love.graphics.newFont('assets/fonts/EncodeSansSemiCondensed-Black.ttf', 32)
+    self.font = love.graphics.newFont('assets/fonts/VT323-Regular.ttf', 24)
 end
 
 function Instructions:update(dt)
-
     if love.keyboard.wasPressed('return') then
         gStateStack:pop()
         gStateStack:push(MainMenu())
     end
-
 end
 
 function Instructions:render()
+    local screenWidth, screenHeight = love.graphics.getDimensions() -- Get screen dimensions
 
-    love.graphics.setColor(1, 1, 1, 1)
-    love.graphics.print("Instructions State",100,100)
-   -- love.graphics.draw(self.control, 0, 0)
-    -- love.graphics.print("Hello guys testing",200,200)
+    -- Draw the background image to fill the entire screen
+    love.graphics.draw(self.background, 0, 0, 0, screenWidth / self.background:getWidth(), screenHeight / self.background:getHeight())
+
+    -- Title with larger size
+    love.graphics.setColor(255, 255, 255, 255)
+    love.graphics.setFont(self.titleFont)
+    love.graphics.print("Instructions", 300, 50)
+
+    -- Instruction 1 with larger size
+    love.graphics.setColor(255, 255, 255, 255)
+    love.graphics.setFont(self.font)
+    love.graphics.print("Welcome to the city!", 50, 100)
+
+    -- Instruction 2 with larger size
+    love.graphics.setColor(255, 255, 255, 255)
+    love.graphics.print("Your mission is to find and fix renewable energy resources like solar panels and windmills.", 50, 150)
+
+    -- Instruction 3 with larger size
+    love.graphics.setColor(255, 255, 255, 255)
+    love.graphics.print("Use the arrow keys (up, down, left, right) to move around.", 50, 200)
+
+    -- Additional Instructions with larger size
+    love.graphics.setColor(255, 255, 255, 255)
+    love.graphics.print("Additional Instructions:", 50, 250)
+
+    -- Instruction 4 with larger size
+    love.graphics.setColor(255, 255, 255, 255)
+    love.graphics.print("1. Search for renewable energy resources like solar panels and windmills in the city.", 50, 300)
+
+    -- Instruction 5 with larger size
+    love.graphics.setColor(255, 255, 255, 255)
+    love.graphics.print("2. Interact with these resources to repair and activate them.", 50, 350)
+
+    -- Instruction 6 with larger size
+    love.graphics.setColor(255, 255, 255, 255)
+    love.graphics.print("3. Keep an eye on your energy level to complete the mission successfully.", 50, 400)
+
+    -- Press Enter to Continue with larger size
+    love.graphics.setColor(255, 255, 255, 255)
+    love.graphics.print("Press Enter to Continue to the Main Menu", 200, 500)
 end
